6b68056640939729c378bd5ab790b163f30bfb8c
eabra <emab21@student.bth.se>
2024-03-20 14:54:16 +0100

    Finish Create Note

---
+++ src/NewNote.tsx
@@ -0,0 +1,21 @@
+import { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
+
+type newNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (newTag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: newNoteProps) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+}
